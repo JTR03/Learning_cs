ad22dd200770e52232207b495e9191c47b7e6455
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-04-13 11:20:36 +0200

    hello world

---
+++ HelloWord/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is Your Name?");
+string name = Console.ReadLine();
+Console.WriteLine("Well Hello There, " + name);
