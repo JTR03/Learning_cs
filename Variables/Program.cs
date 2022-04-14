a826b3a842aaf95d793af18e0b7c8cb5049e17a8
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-04-14 23:32:01 +0200

    declaring and printing variables

---
+++ Variables/Program.cs
@@ -0,0 +1,14 @@
+﻿// Program that prints out different variables and data types
+
+int age = 10;
+bool isAwesome = true;
+char initial = 'J';
+// decimal GPA = 52.4;
+float f = 2.0f;
+string name = "Junior Calmo";
+
+// implicit variables
+var i = 125;
+var str = "Structure of the world";
+
+Console.WriteLine("{0},{1},{2},{3},{4},{5},{6}", age,isAwesome,initial,i,f,name,str);
