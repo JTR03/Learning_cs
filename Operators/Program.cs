b595f10a8e112a8e23b8d9061754fbc4c770db8b
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-05-12 08:30:41 +0200

    c sharp operators

---
+++ Operators/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// operators
+int x = 10, y = 5;
+string a ="alphabets", b="letters";
+
+// basic math operators
+Console.WriteLine((x/y)*x);
+Console.WriteLine(a + b);
+
+// increment and decrement
+x++;
+y--;
+
+// shorthand operator a = a + b
+a += b;
+
+// logical Operators 
+Console.WriteLine(x > y && y >= 5);
+Console.WriteLine(x > y || y >= 5);
+
+// null-coalescing operator
+string str = null;
+
+Console.WriteLine(str ?? "unknown string");
+str ??= "new string";
+Console.WriteLine(str);
