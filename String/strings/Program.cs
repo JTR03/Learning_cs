898be674718dd2d37e9e06c1b8690b052f8042aa
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-05-12 12:02:39 +0200

    organise folders

---
+++ String/strings/Program.cs
@@ -0,0 +1,47 @@
+﻿// string operations
+
+string outstr;
+string str1 = "the quick brown fox jumped over the lazy dog";
+string str2 = "this is some string";
+string str3 = "THIS is a STRING";
+string[] strs = {"one","two","three","four"};
+
+Console.WriteLine(str1.Length);
+Console.WriteLine(str1[12]);
+foreach (char c in str1)
+{
+    Console.Write(c);
+    if (c == 'j')
+    {
+        Console.WriteLine();
+        break;
+    }
+}
+
+outstr = String.Concat(strs);
+Console.WriteLine(outstr);
+
+outstr = String.Join(".",strs);
+Console.WriteLine(outstr);
+
+outstr = String.Join("---",strs);
+Console.WriteLine(outstr);
+
+// string comparition
+
+int result = String.Compare(str2, "this is some text");
+Console.WriteLine(result);
+
+bool isEqual = str2.Equals(str3);
+Console.WriteLine(isEqual);
+
+// String search
+Console.WriteLine(str1.IndexOf('e'));
+Console.WriteLine(str1.IndexOf("fox"));
+Console.WriteLine(str1.LastIndexOf('e'));
+Console.WriteLine(str1.LastIndexOf("lazy"));
+
+outstr = str1.Replace("fox","cat");
+Console.WriteLine(outstr);
+Console.WriteLine(outstr.IndexOf("fox"));
+
---
+++ basics/Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿// Program that prints out different variables and data types
+
+int age = 10;
+bool isAwesome = true;
+char initial = 'J';
+// decimal GPA = 52.4;
+float f = 2.0f;
+string name = "Junior Calmo";
+
+// implicit variables
+var i = 125;
+var str = "Structure of the world";
+
+// array of values
+int[] vals = new int[5];
+string[] strs = {"one","two","three"};
+
+Console.WriteLine("{0},{1},{2},{3},{4},{5},{6}", age,isAwesome,initial,i,f,name,str);
+
+// implicit conversion
+long bignum;
+bignum = i;
+
+// explicit conversion
+float i_to_f = (float)i;
+Console.WriteLine("{0}",i_to_f);
+
+int f_to_i = (int)f;
+Console.WriteLine("{0}",f_to_i);
+
