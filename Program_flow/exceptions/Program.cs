0ede7d2976df549982141e51053506479149d87f
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-05-12 11:02:08 +0200

    exception handling

---
+++ Program_flow/exceptions/Program.cs
@@ -0,0 +1,17 @@
+﻿int x = 100;
+int y = 0;
+int result;
+
+try
+{
+    result = x/y;
+    Console.WriteLine($"result is: {result}");
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine("You can't divide by zero");
+    Console.WriteLine(e.Message);
+}
+finally{
+    Console.WriteLine("code always runs");
+}
